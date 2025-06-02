818cf3e2d80f8c5c7f7c7d15da24cfc0abd8c452
RishabSolemn <rishabsolemn@gmail.com>
2025-06-02 23:08:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,3 +54,9 @@ def handle_scan(data):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
